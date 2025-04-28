f045dc35fa3eaa64d042e1cc76a74cc900d8e63e
riaz37 <riaz37.ipe@gmail.com>
2025-04-28 20:24:55 +0600

    fix: modified build config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: ".next",
+  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
